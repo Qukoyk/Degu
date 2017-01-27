0efba0cb5fb27141e325cd65c9fcc70bd80b2155
Qukoyk <925767307@qq.com>
2017-01-27 14:34:11 +0900

    Add descriptions
    Added descriptions of actogram.py

--- actogram.py
+++ actogram.py
@@ -1,26 +1,27 @@
+#!/usr/bin/env python3
+
 import matplotlib.pyplot as plt
-import matplotlib.spines as spines
 import os,xlrd
 import numpy as np
 
-
+#directory & column
 data = xlrd.open_workbook('C:/Users/Qukoyk/Desktop/201611-12.xlsx')
-table = data.sheet_by_index(0)
-ncols = table.ncols
+table = data.sheet_by_index(0) #sheet of data
+ncols = table.ncols #get the number of columns
 
-
+#number of subplot
 z = 1
 
-for i in range(ncols):
-    x = table.col_values(i)
-    y = np.arange(len(x))
-    plt.subplot(ncols,1,z)
-    plt.bar(y,x,color='k')
-    plt.xticks(visible=False)
-    plt.yticks(visible=False)
-    plt.subplots_adjust(hspace=0)
-    # plt.subplots_adjust(left=0.30,right=0.7,hspace=0)
-    plt.ylim(0,50)
-    z = z+1
+#
+for i in range(ncols): #circulation of bar plot
+    x = table.col_values(i) #data in columns
+    y = np.arange(len(x)) #set groups as lenth of x-tick
+    plt.subplot(ncols,1,z) #draw subplot(all subplot,draw in 1 column,this subplot)
+    plt.bar(y,x,color='k') #draw bar plot
+    plt.xticks(visible=False) #xticks invisible
+    plt.yticks(visible=False) #yticks invisible
+    plt.subplots_adjust(hspace=0) #no places between subplots
+    plt.ylim(0,50) #set limitation of y-tick to 50
+    z = z+1 #number of subplot +1  &  into the next circulation
 
-plt.show()
+plt.show() #show the actogram, takes about 1 minute under Raspberry Pi 3B
